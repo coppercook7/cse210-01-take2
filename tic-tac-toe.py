e95188f1e9764287546a906cc0c0dd6d1a0a72b3
coppercook7 <coppercook7@byui.edu>
2022-01-20 19:43:22 -0700

    eddited the begginging of the program

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,9 @@
-print('hello')
+def main():
+    player = next_player("")
+    board = create_game()
+    while not (winner(board) or draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Good game. Thanks for playing!") 
